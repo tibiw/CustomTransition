b6986119b6a0aab39f676f8e0860d1a140f36649
tibiw <tibor.waxmann@gmail.com>
2022-02-24 19:42:06 +0200

    Initial Commit

---
+++ CustomTransition/CustomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionApp.swift
+//  CustomTransition
+//
+//  Created by Tibor Waxmann on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
